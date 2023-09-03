66033ceff14271b68004f9e5dbe6b844a116d78b
Fam1n0 <142743561+Fam1n0@users.noreply.github.com>
2023-09-03 10:55:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function openThirdEye() {
 function closeModal() {
     document.getElementById('awakening-modal').style.display = 'none';  // Hide the modal
 }
+
+let isEyeOpened = false;
+
+function toggleThirdEye() {
+    const thirdEye = document.getElementById('third-eye');
+    const modal = document.getElementById('awakening-modal');
+
+    if (isEyeOpened) {
+        // If the third eye is opened, revert everything to its initial state:
+        thirdEye.style.width = '50px';
+        thirdEye.style.height = '50px';
+        document.body.style.backgroundColor = '#111';
+        document.getElementById('game-container').style.backgroundImage = 'url("https://fam1n0.github.io/ThirdEye/background3rdeye.jpg")';
+        modal.style.display = 'none';
+        isEyeOpened = false;
+    } else {
+        // If the third eye is not opened, proceed with opening it:
+        thirdEye.style.width = '100px';
+        thirdEye.style.height = '100px';
+        setTimeout(() => {
+            document.body.style.backgroundColor = '#eee';
+            document.getElementById('game-container').style.backgroundImage = 'url("https://fam1n0.github.io/ThirdEye/path-to-universe-image.jpg")';
+            modal.style.display = 'block';
+        }, 1000);
+        isEyeOpened = true;
+    }
+}

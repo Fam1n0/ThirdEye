7769718b6d8f295dae39bb4ce9fe4bcd727f1966
Fam1n0 <142743561+Fam1n0@users.noreply.github.com>
2023-09-03 09:19:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+
 function openThirdEye() {
     const thirdEye = document.getElementById('third-eye');
+    const modal = document.getElementById('awakening-modal');
 
     thirdEye.style.width = '100px';
     thirdEye.style.height = '100px';
 
-    // Simulate the "opening" of the third eye
+    // Simulate the "opening" of the third eye and then show the modal
     setTimeout(() => {
         document.body.style.backgroundColor = '#eee';
         document.getElementById('game-container').style.backgroundImage = 'url("path-to-universe-image.jpg")';
-
-        alert('You have awakened the inner universe!');
+        
+        modal.style.display = 'block';  // Show the modal
     }, 1000);
+}
+
+function closeModal() {
+    document.getElementById('awakening-modal').style.display = 'none';  // Hide the modal
 }
